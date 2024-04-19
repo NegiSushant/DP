f566ba279f5d4953682d9dedb1ea5a1d5e71964f
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2024-04-19 11:03:55 +0530

    Update DiameterOfTree.java

--- DP on Tree/DiameterOfTree.java
+++ DP on Tree/DiameterOfTree.java
@@ -3,6 +3,11 @@ take any 2 leaf node
 find the number of node along longest path between two leafs.
 */
 public class DiameterOfTree{
+  public static void main(String[] args){
+    int res = Integer.MAX_VALUE;
+    System.out.println(Solve(root, res));
+  // return res;
+  }
   public static int Solve(Node root, int res){
     if(root == null){
       return 0;
